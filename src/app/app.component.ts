b8af29dcbeeaaee0db789bb354f1b186fea27502
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2025-03-20 13:06:37 +0100

    Implementa componente de rodapé e atualiza o componente principal para utilizá-lo. Adiciona link para download do CV no componente de herói e ajusta estilos nos componentes de contato e herói.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    RouterOutlet,
     NavbarComponent,
     HeroComponent,
     ProjectsComponent,
     SkillsComponent,
     ContactComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
