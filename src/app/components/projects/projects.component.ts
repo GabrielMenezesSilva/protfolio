651e51a91c5f24af9efa5a93456085a4a8f5bd69
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2025-03-20 16:15:44 +0100

    Atualiza o título da página para 'Gabriel Menezes - Développeur Web', adiciona novos ícones e favicon em formatos SVG e PNG, e inclui um manifesto para web app. Também modifica a descrição e as tecnologias do projeto 'Site WordPress', além de comentar a seção do projeto 'Plateforme E-commerce'.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -22,9 +22,9 @@ export class ProjectsComponent {
     {
       title: 'Site WordPress',
       description:
-        "Site professionnel développé avec WordPress, axé sur l'acquisition de clients pour le développement web.",
+        'Site professionnel développé avec WordPress et Elementor, axé sur la présentation de services de développement web. Le site comprend des sections dynamiques pour le portfolio. Design responsive et optimisé pour le SEO, garantissant une expérience utilisateur fluide sur tous les appareils.',
       image: 'assets/projects/wordpress-portfolio.png',
-      technologies: ['WordPress', 'Elementor'],
+      technologies: ['WordPress', 'Elementor', 'SEO', 'Design Responsive'],
       demo: 'https://gabriel-menezes.com',
     },
     {
@@ -33,24 +33,24 @@ export class ProjectsComponent {
         "Développement d'une plateforme de gestion scolaire adaptée aux besoins spécifiques d'une institution éducative. Fonctionnalités clés : gestion des profils d'étudiants, système de chat de groupe par formation, calendrier des cours personnalisable, portail de cours enregistrés. Application PWA avec Firebase pour la gestion des données et des utilisateurs en temps réel.",
       image: 'assets/projects/nomades.png',
       technologies: [
+        'PWA',
         'Angular',
         'Firebase',
         'TypeScript',
         'HTML',
         'CSS',
         'API REST',
-        'PWA',
       ],
       demo: 'https://zoomnomades-e95cd.web.app/login',
     },
-    {
-      title: 'Plateforme E-commerce',
-      description:
-        "Plateforme e-commerce complète avec panier d'achat, paiements et zone administrative.",
-      image: 'assets/projects/ecommerce.jpg',
-      technologies: ['Angular', 'Node.js', 'PostgreSQL', 'Stripe'],
-      github: 'https://github.com/seu-usuario/ecommerce',
-      demo: 'https://demo-ecommerce.com',
-    },
+    // {
+    //   title: 'Plateforme E-commerce',
+    //   description:
+    //     "Plateforme e-commerce complète avec panier d'achat, paiements et zone administrative.",
+    //   image: 'assets/projects/ecommerce.jpg',
+    //   technologies: ['Angular', 'Node.js', 'PostgreSQL', 'Stripe'],
+    //   github: 'https://github.com/seu-usuario/ecommerce',
+    //   demo: 'https://demo-ecommerce.com',
+    // },
   ];
 }
