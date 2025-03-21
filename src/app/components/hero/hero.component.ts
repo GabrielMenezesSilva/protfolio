ae93eeeb65b59df641409fcc26e80b8ef415d81c
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2025-03-21 17:52:13 +0100

    Adiciona um vídeo de fundo ao componente de herói, implementa estilos para o vídeo e ajusta a estrutura do componente, melhorando a apresentação visual e a interatividade.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,10 +14,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss', '../../../styles/animations.scss'],
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit, AfterViewInit {
+  @ViewChild('videoBackground') videoBackground!: ElementRef<HTMLVideoElement>;
+
   name = 'Gabriel Menezes';
   title = 'Développeur Web';
   description =
-    "Je transforme des défis techniques en solutions efficaces, en combinant expertise en développement web et approche agile.";
+    'Je transforme des défis techniques en solutions efficaces, en combinant expertise en développement web et approche agile.';
   imagePath = 'assets/images/hero2.png';
+
+  ngOnInit() {
+    // Inicialização
+  }
+
+  ngAfterViewInit() {
+    const video = this.videoBackground.nativeElement;
+
+    video.addEventListener('loadeddata', () => {
+      video.play().catch((error) => {
+        console.error('Erro ao reproduzir vídeo:', error);
+      });
+    });
+  }
 }
