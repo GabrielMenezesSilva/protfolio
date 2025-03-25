aa3e17da8bd9f95538b1b233d363a133038476d7
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2025-03-25 16:13:21 +0100

    Atualiza o nome no componente Hero para 'Gabriel Menezes', removendo a versão do autor.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./hero.component.scss', '../../../styles/animations.scss'],
 })
 export class HeroComponent implements OnInit {
-  name = 'Gabriel Menezes v1';
+  name = 'Gabriel Menezes';
   title = 'Développeur Web';
   description =
     'Je transforme des défis techniques en solutions efficaces, en combinant expertise en développement web et approche agile.';
