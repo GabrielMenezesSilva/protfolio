7b75cca6dbd0281a444b2e08f90b58006a453b4b
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2025-03-25 15:58:37 +0100

    Atualiza o script de build no package.json para usar a configuração de produção. Adiciona o componente Footer ao app.component.ts, melhorando a estrutura do layout da aplicação.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     NavbarComponent,
+    FooterComponent,
     HeroComponent,
     ProjectsComponent,
     SkillsComponent,
     ContactComponent,
-    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
