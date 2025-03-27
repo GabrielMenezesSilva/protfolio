1eec5507128f5d25b25722b0d9bb023185edcbec
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2025-03-27 14:09:24 +0100

    Atualiza o título no componente Hero de 'Développeur Web' para 'Développeur Full Stack', refletindo uma mudança na descrição profissional.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -11,7 +11,7 @@ import { CloudinaryService } from '../../services/cloudinary.service';
 })
 export class HeroComponent implements OnInit {
   name = 'Gabriel Menezes';
-  title = 'Développeur Web';
+  title = 'Développeur Full Stack';
   description =
     'Je transforme des défis techniques en solutions efficaces, en combinant expertise en développement web et approche agile.';
   imagePath = 'assets/images/hero2.png';
